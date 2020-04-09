311c6bdae39db75e28b1be5dde9a4ac639c7c4a4
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-09 12:57:47 +0100

    Router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Router from './Router';
 
 function App() {
   return (
     <div className='App'>
-      <div className='container'>hello world</div>
+      <Router />
     </div>
   );
 }
