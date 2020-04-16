cfe30a52b59c2a36b54060aa1f69342765a04890
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-16 21:16:41 +0100

    push

--- src/Components/Google/Videos.js
+++ src/Components/Google/Videos.js
@@ -4,9 +4,11 @@ import React, { Component } from 'react';
 export class Videos extends Component {
   render() {
     const { items } = this.props;
-    if (items.length > 0) {
+    if (items.length === 0) {
+      return <div>loading</div>;
+    } else {
       console.log(items);
-      return <div>okay</div>;
+      return <div>confirm</div>;
     }
   }
 }
