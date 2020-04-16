5426c3f48e04cc8e51078258ed8573b9d00fa1be
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-16 20:50:09 +0100

    push

--- src/Components/Google/Videos.js
+++ src/Components/Google/Videos.js
@@ -1,14 +1,33 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React, { Component } from 'react';
 
 export class Videos extends Component {
   render() {
     const { items } = this.props;
-    console.log(items);
 
     if (items.length === 0) {
       return <div>loading</div>;
     } else {
-      return <div>Confirm</div>;
+      items.forEach(item => {
+        const videoId = item.snippet.resourceId.videoId;
+
+        return (
+          // eslint-disable-next-line react/jsx-no-comment-textnodes
+          <div className='col-s3'>
+            {
+              // eslint-disable-next-line jsx-a11y/iframe-has-title
+            }
+            <iframe
+              width='100%'
+              height='auto'
+              src={`https://www.youtube.com/embed/${videoId}`}
+              frameForder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowFullScreen
+            ></iframe>
+          </div>
+        );
+      });
     }
   }
 }
