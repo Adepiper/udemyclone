b2c8933153ce308bf93d1e4e5b3ed9e8162d327a
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-16 21:11:31 +0100

    push

--- src/Components/Google/Videos.js
+++ src/Components/Google/Videos.js
@@ -4,15 +4,9 @@ import React, { Component } from 'react';
 export class Videos extends Component {
   render() {
     const { items } = this.props;
-
-    if (items.length === 0) {
-      return <div>loading</div>;
-    } else {
-      items.map(item => {
-        const videoId = item.snippet.resourceId.videoId;
-        console.log(videoId);
-        return <div className='col-s3'>okay</div>;
-      });
+    if (items.length > 0) {
+      console.log(items);
+      return <div>okay</div>;
     }
   }
 }
