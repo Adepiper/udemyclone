2b5660d5f4e29484bd06ae4ab6746fc25caf268f
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-16 12:47:08 +0100

    channel

--- src/Components/Google/Channeldata.js
+++ src/Components/Google/Channeldata.js
@@ -3,21 +3,24 @@ import React, { Component } from 'react';
 export class Channeldata extends Component {
   render() {
     const { channel } = this.props;
-
-    return (
-      <div>
-        <ul className='collection'>
-          <li className='collection-item'>{channel.snippet.title}</li>
-          <li className='collection-item'>${channel.id}</li>
-          <li className='collection-item'>
-            {channel.statistics.subscriberCount}
-          </li>
-          <li className='collection-item'>{channel.statistics.videoCount}</li>
-          <li className='collection-item'>{channel.statistics.viewCount}</li>
-        </ul>
-        <p>{channel.snippet.description}</p>
-      </div>
-    );
+    if (channel) {
+      return (
+        <div>
+          <ul className='collection'>
+            <li className='collection-item'>{channel.snippet.title}</li>
+            <li className='collection-item'>${channel.id}</li>
+            <li className='collection-item'>
+              {channel.statistics.subscriberCount}
+            </li>
+            <li className='collection-item'>{channel.statistics.videoCount}</li>
+            <li className='collection-item'>{channel.statistics.viewCount}</li>
+          </ul>
+          <p>{channel.snippet.description}</p>
+        </div>
+      );
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
