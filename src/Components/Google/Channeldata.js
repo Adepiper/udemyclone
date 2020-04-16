fdbe4005f4012647017be41bb972999467cdf3bb
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-16 13:20:24 +0100

    push

--- src/Components/Google/Channeldata.js
+++ src/Components/Google/Channeldata.js
@@ -4,8 +4,23 @@ export class Channeldata extends Component {
   render() {
     const { channel } = this.props;
     console.log(channel);
-    if (channel) {
-      return <div></div>;
+    if (channel.length > 0) {
+      return (
+        <div>
+          <ul className='collection'>
+            <li className='collection-item'>${channel.snippet.title}</li>
+            <li className='collection-item'>${channel.id}</li>
+            <li className='collection-item'>
+              {channel.statistics.subscriberCount}
+            </li>
+            <li className='collection-item'>
+              ${channel.statistics.videoCount}
+            </li>
+            <li className='collection-item'>${channel.statistics.viewCount}</li>
+          </ul>
+          <p>${channel.snippet.description}</p>
+        </div>
+      );
     } else {
       return <div>Loading</div>;
     }
