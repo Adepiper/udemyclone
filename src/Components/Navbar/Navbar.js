fbfd5f1b4d11cf53fac9640260ddfc21fb8b27a4
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-17 01:39:46 +0100

    push

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -15,7 +15,8 @@ export class Navbar extends Component {
   loginIn = () => {
     const { google } = this.props;
     google.initClient().then(() => {
-      console.log(gapi.auth2.getAuthInstance().currentUser);
+      gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+      this.updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
       google.handleAuthClick();
     });
     google.handleAuthClick();
@@ -24,17 +25,100 @@ export class Navbar extends Component {
   logOut = () => {
     const { google } = this.props;
     google.initClient().then(() => {
-      console.log(gapi.auth2.getAuthInstance().currentUser);
       google.handleSignoutClick();
+      this.setState({
+        isSignedIn: null
+      });
     });
     google.handleSignoutClick();
   };
 
+  updateSignInStatus = isSignedIn => {
+    if (isSignedIn) {
+      this.setState({
+        isSignedIn: isSignedIn
+      });
+    } else {
+      return null;
+    }
+  };
   toggleBtn() {
     document.body.classList.toggle('show-nav');
   }
 
   render() {
+    const { isSignedIn } = this.state;
+
+    if (isSignedIn) {
+      return (
+        <div>
+          <div className='topnav' id='myTopnav'>
+            <ul>
+              <li>
+                <a href='#' className='icon' onClick={this.toggleBtn}>
+                  &#9776;
+                </a>
+              </li>
+              <li className='home'>
+                <Link to='/'>Piperland</Link>
+              </li>
+              <li>
+                <form className='form'>
+                  <input
+                    type='text'
+                    placeholder='Search Courses'
+                    className='formControl'
+                  />
+                  <button className=''>
+                    <i className='fa fa-search'></i>
+                  </button>
+                </form>
+              </li>
+              <li className='float-right'>
+                <button onClick={this.logOut}>Logout</button>
+              </li>
+              <li className='float-right'>
+                <Link to='/courses'>Register</Link>
+              </li>
+              <li className='float-right'>
+                <Link to='/instructor'>Instructor</Link>
+              </li>
+              <li className='float-right'>
+                <Link to='/courses'>Courses</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='responsive'>
+            <ul>
+              <div>
+                <li className=''>
+                  <button onClick={this.logOut}>Logout</button>
+                </li>
+              </div>
+
+              <li>
+                <form className='form'>
+                  <input
+                    type='text'
+                    placeholder='Search Courses'
+                    className='formControl'
+                  />
+                  <button className=''>
+                    <i className='fa fa-search'></i>
+                  </button>
+                </form>
+              </li>
+              <li className=''>
+                <Link to='/instructor'>Instructor</Link>
+              </li>
+              <li className=''>
+                <Link to='/courses'>Courses</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
     return (
       <div>
         <div className='topnav' id='myTopnav'>
@@ -63,9 +147,6 @@ export class Navbar extends Component {
               <button onClick={this.loginIn}>Login</button>
             </li>
             <li className='float-right'>
-              <button onClick={this.logOut}>Logout</button>
-            </li>
-            <li className='float-right'>
               <Link to='/courses'>Register</Link>
             </li>
             <li className='float-right'>
@@ -80,10 +161,7 @@ export class Navbar extends Component {
           <ul>
             <div>
               <li className=''>
-                <Link to='/instructor'>Login</Link>
-              </li>
-              <li className=''>
-                <Link to='/instructor'>Register</Link>
+                <button onClick={this.loginIn}>Login</button>
               </li>
             </div>
 
