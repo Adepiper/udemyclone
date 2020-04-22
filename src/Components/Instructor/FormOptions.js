772b195f1655aaee9ac1ad75bccf513acfb6233f
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-22 18:26:50 +0100

    push

--- src/Components/Instructor/FormOptions.js
+++ src/Components/Instructor/FormOptions.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function FormOptions() {
-  const { getChannel, createChannel } = this.props;
-  return (
-    <div className='formoptions'>
-      <button onClick={getChannel}>Find Channel</button>
-      <p>Or</p>
-      <button onClick={createChannel}>Create Channel</button>
-    </div>
-  );
+export class FormOptions extends Component {
+  render() {
+    const { getChannel, createChannel } = this.props;
+    return (
+      <div className='formoptions'>
+        <button onClick={getChannel}>Find Channel</button>
+        <p>Or</p>
+        <button onClick={createChannel}>Create Channel</button>
+      </div>
+    );
+  }
 }
 
 export default FormOptions;
