121f710d45382b7abe2b9c43b8b86240263b1ffb
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-23 01:49:12 +0100

    push

--- src/Components/Instructor/InstructorDetails.js
+++ src/Components/Instructor/InstructorDetails.js
@@ -3,22 +3,29 @@ import React, { Component } from 'react';
 export class InstructorDetails extends Component {
   render() {
     const { channel } = this.props;
-    return (
-      <div>
-        <ul className='collection'>
-          <li className='collection-item'>{channel[0].snippet.title}</li>
-          <li className='collection-item'>{channel[0].id}</li>
-          <li className='collection-item'>
-            {channel[0].statistics.subscriberCount}
-          </li>
-          <li className='collection-item'>
-            {channel[0].statistics.videoCount}
-          </li>
-          <li className='collection-item'>{channel[0].statistics.viewCount}</li>
-        </ul>
-        <p>{channel[0].snippet.description}</p>
-      </div>
-    );
+    console.log(channel);
+    if (channel.length > 0) {
+      return (
+        <div>
+          <ul className='collection'>
+            <li className='collection-item'>{channel[0].snippet.title}</li>
+            <li className='collection-item'>{channel[0].id}</li>
+            <li className='collection-item'>
+              {channel[0].statistics.subscriberCount}
+            </li>
+            <li className='collection-item'>
+              {channel[0].statistics.videoCount}
+            </li>
+            <li className='collection-item'>
+              {channel[0].statistics.viewCount}
+            </li>
+          </ul>
+          <p>{channel[0].snippet.description}</p>
+        </div>
+      );
+    } else {
+      return false;
+    }
   }
 }
 
