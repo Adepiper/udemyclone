fc081caf9702d2b06b16c46c18623606b02ed938
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-04-24 02:16:59 +0100

    push

--- src/Router.js
+++ src/Router.js
@@ -151,22 +151,17 @@ export class Router extends Component {
         this.setState({
           channels: channels
         });
+        const playListId = channels.map(channel => {
+          return channel.contentDetails.relatedPlaylists.uploads;
+        });
+        console.log(playListId);
+        this.requestVideoPlaylist(playListId);
       })
       .catch(err => {
         console.log(err);
       });
-    const { channels } = this.state;
+    // const { channels } = this.state;
     //console.log(channels);
-    if (channels.length === 0) {
-      return false;
-    } else {
-      console.log(channels);
-      const playListId = channels.map(channel => {
-        return channel.contentDetails.relatedPlaylists.uploads;
-      });
-      console.log(playListId);
-      this.requestVideoPlaylist(playListId);
-    }
   };
 
   getChannelData = () => {
