f3fa16e6cf863f38bdc24f6999a6e1dc294958c2
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-05-04 14:56:13 +0100

    debug

--- src/Router.js
+++ src/Router.js
@@ -268,7 +268,7 @@ export class Router extends Component {
     axios.post(`${db}/unrefinedVideos`, videosData).then(res => {
       const data = res.data;
       let refinedData = data.map((video, index) => {
-        return video[index];
+        return { ...video };
       });
       // let refinedData = data.map(video => {
       //   video = JSON.parse(JSON.stringify(video));
